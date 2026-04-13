5f50bd940e1a67f6b2df2c11a078565d1bfb611b
Ayaan496 <mushtaqa@vcu.edu>
2026-04-12 20:41:49 -0400

    grade.js is now repurposed for the assignment

--- grade.js
+++ grade.js
@@ -2,33 +2,58 @@
 var readLineSync = require('readline-sync')
 
 
-var name = readLineSync.question("Name?")
+var average = readLineSync.questionFloat("Input your current grade average: ")
+const finalWeight = 0.25
+while (true){
+    var finalScore = readLineSync.questionInt("Input your final exam score: ")
+    //finalgrade = current *.75 + final *.25
+    testScore = (average * .75) + (finalScore * .25)
+    var letterGrade;
+    
+    
+    if (testScore < 0 || testScore >100){
+        console.log("Invalid Score")
+        return
+    }
+    else if(testScore > 89.5){
+        letterGrade = "A"
+    }
+    else if (testScore > 79.5){
+        letterGrade = "B"
+    }
+    else if (testScore > 69.5){
+        letterGrade = "C"
+    }
+    else if (testScore > 59.5){
+        letterGrade = "D"
+    }
+    else{
+        letterGrade = "F"
+    }
 
-var testScore = readLineSync.questionInt("Score?")
+
+    console.log("\nThe hypothetical final score was " + finalScore)
+    console.log("The final course average is " + testScore)
+    console.log("The letter grade is " + letterGrade)
+    if(testScore > average){
+        console.log("The grade improved, good job")
+    }
+    else if(testScore == average){
+        console.log("The grade stayed the same")
+
+    }
+    else{
+        console.log("The grade declined, work harder")
+    }
+    console.log("Please input the next score \n\n")
+}
 
 
-var message = "The student" + name + "received a score of " + testScore
-console.log(message)
-if (testScore < 0 || testScore >100){
-    message ="Invalid Score"
-}
-else if(testScore > 89.5){
-    message = "Which earned an A"
-}
-else if (testScore > 79.5){
-    message = "Which earned a B"
-}
-else if (testScore > 69.5){
-    message = "Which earned a C"
-}
-else if (testScore > 59.5){
-    message = "Which earned a D"
-}
-else{
-    message = "Which earned an F"
-}
 
-console.log(message)
+
+
+
+
 
 
 
