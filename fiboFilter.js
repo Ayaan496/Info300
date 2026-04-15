2b83c681bb600f859aa6274f910b4918234ce730
Ayaan496 <mushtaqa@vcu.edu>
2026-04-14 23:51:37 -0400

    added fiboFilter.js

---
+++ fiboFilter.js
@@ -0,0 +1,17 @@
+var readLineSync = require('readline-sync')
+
+var n = readLineSync.questionInt("Input the number of Fibonacci numbers to generate: ")
+
+var fibonacci = [0,1];
+var oddFibo = []
+
+for (let i = 2; i < n; i++){
+    fibonacci[i] = fibonacci[i-1] + fibonacci[i - 2]
+    if(fibonacci[i] % 2 == 1){
+        oddFibo.push(fibonacci[i])
+    }
+}
+console.log("Fibonacci Sequence: ")
+console.log(fibonacci)
+console.log("Odd Fibonacci Numbers: ")
+console.log(oddFibo)
